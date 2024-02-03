3d86dbb99e47522344f8190ad9311844011cdcc8
dcrame2 <dcrame2@gmail.com>
2024-02-02 22:41:13 -0600

    nah nvm

--- screens/UserSetup/Index.tsx
+++ screens/UserSetup/Index.tsx
@@ -11,7 +11,7 @@ export default function UserSetup() {
   const [users, setUsers] = useState([]);
 
   let socket: any;
-  socket = io("https://irish-poker.onrender.com");
+  socket = io("http://localhost:3001");
 
   const handleJoin = () => {
     if (userName !== "" && roomId !== "") {
