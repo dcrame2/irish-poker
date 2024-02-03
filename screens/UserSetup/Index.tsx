173222c7d21d50e5a6b32d89f10c75ad1f47a961
dcrame2 <dcrame2@gmail.com>
2024-02-02 20:34:52 -0600

    update

--- screens/UserSetup/Index.tsx
+++ screens/UserSetup/Index.tsx
@@ -11,7 +11,7 @@ export default function UserSetup() {
   const [users, setUsers] = useState([]);
 
   let socket: any;
-  socket = io();
+  socket = io("http://localhost:3001");
 
   const handleJoin = () => {
     if (userName !== "" && roomId !== "") {
