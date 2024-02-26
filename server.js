811dce8d366186d5bc13b5e79c98f5f282f92516
dcrame2 <dcrame2@gmail.com>
2024-02-25 20:22:43 -0600

    update

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const httpServer = http.createServer(app);
 const io = new Server(httpServer, {
   cors: {
     // origin: "https://irishpokeronline.netlify.app",
-    origin: "http://irish-poker.com",
+    origin: "https://irish-poker.com",
     // origin: "http://localhost:3000",
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
